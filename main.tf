8e86a3e488edcff58882d514ecbd2517cb162ca6
Richard Dhali <60823958+RichardDhali@users.noreply.github.com>
2022-11-03 22:31:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@
 resource "aws_instance" "ELK-Deploy" {
     ami = "ami-09d3b3274b6c5d4aa"
     count = "1"
-    key_name   = "Jenkins"
+    key_name   = "abelkp"
     instance_type = "t3.large"
     security_groups = ["launch-wizard-1"]
     tags = {
-        Name = "ELK-Stack-deploy"
+        Name = "ELK-Stack"
     } 
     user_data = file("elk.sh")
    
